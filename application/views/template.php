180026abd2434033b7c64d0428b401810c8b3e50
Muhammad Yusron <muhammadyusron17@gmail.com>
2020-03-13 22:43:22 +0700

    up

--- application/views/template.php
+++ application/views/template.php
@@ -171,9 +171,9 @@ background-color:#f9f9f9;z-index: 3;">
                     <div class="footer-widget nav_menus_widget">
                         <h4 class="widget-title" >Our Customer</h4>
                         <ul>
-                            <li> <a href="#" ><i class="fas fa-chevron-right"></i> <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/Temas.png" alt="temas logo"></a></li>
+                            <li> <a href="#" > <img style="max-height: 60px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/Temas.png" alt="temas logo"></a></li>
                            
-                            <li> <a href="#" ><i class="fas fa-chevron-right"></i> <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/samudra-indonesia.png" alt="samudera indonesia logo"></a></li>
+                            <li> <a href="#" > <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/samudra-indonesia.png" alt="samudera indonesia logo"></a></li>
 
                             
                         </ul>
@@ -184,9 +184,9 @@ background-color:#f9f9f9;z-index: 3;">
                         <h4 class="widget-title" > </h4><br>
                         <ul>     
                             
-                            <li> <a href="#" ><i class="fas fa-chevron-right"></i> <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/pelindo1.png" alt="pelindo 1 logo"></a></li>
-                             <li> <a href="#" ><i class="fas fa-chevron-right"></i> <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/ipc.png" alt="temas logo"></a></li>
-                            <li> <a href="#" ><i class="fas fa-chevron-right"></i> <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/pelindo3.png" alt="pelindo 3 logo"></a></li>
+                            <li> <a href="#" > <img style="max-height: 90px; max-width: 150px" src="<?=base_url()?>assets/images/ourcustomer/pelindo1.png" alt="pelindo 1 logo"></a></li>
+                             <li> <a href="#" > <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/ipc.png" alt="temas logo"></a></li>
+                            <li> <a href="#" > <img style="max-height: 80px; max-width: 150px" src="<?=base_url()?>assets/images/ourcustomer/pelindo3.png" alt="pelindo 3 logo"></a></li>
 
                           
                             
@@ -198,8 +198,8 @@ background-color:#f9f9f9;z-index: 3;">
                     <div class="footer-widget nav_menus_widget">
                         <h4 class="widget-title" > </h4><br>
                         <ul>                            
-                            <li> <a href="#" ><i class="fas fa-chevron-right"></i> <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/nph.png" alt="nusantara pelabuhan handal logo"></a></li>
-                            <li> <a href="#" ><i class="fas fa-chevron-right"></i> <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/Sinarmas.png" alt="sinarmas logo"></a></li>
+                            <li> <a href="#" > <img style="max-height: 50px; max-width: 100px" src="<?=base_url()?>assets/images/ourcustomer/nph.png" alt="nusantara pelabuhan handal logo"></a></li>
+                            <li> <a href="#" > <img style="max-height: 50px; max-width: 150px" src="<?=base_url()?>assets/images/ourcustomer/Sinarmas.png" alt="sinarmas logo"></a></li>
                             
                         </ul>
                     </div>
